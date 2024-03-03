4cf5c2915fcd3d4a7eecd34935e6807bb73614b2
hbohlen <bohlenhayden@gmail.com>
2024-03-02 20:52:27 -0600

    Add @prisma/client and prisma as dependencies

---
+++ components/AddItemForm.tsx
@@ -0,0 +1,28 @@
+import { useState, FormEvent } from "react";
+
+type AddItemFormProps = {
+  onAddItem: (itemName: string) => void;
+};
+
+const AddItemForm: React.FC<AddItemFormProps> = ({ onAddItem }) => {
+  const [itemName, setItemName] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onAddItem(itemName);
+    setItemName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={itemName}
+        onChange={(e) => setItemName(e.target.value)}
+      />
+      <button type="submit">Add Item</button>
+    </form>
+  );
+};
+
+export default AddItemForm;
---
+++ prisma/migrations/20240303024242_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
