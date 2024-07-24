7a20dcd3428bcb8850f0f9f932a37301b3bcb82c
hbohlen <bohlenhayden@gmail.com>
2024-07-23 21:49:05 -0500

    refactor: Update types.ts to include interfaces for Beverage, Restock, and Count

--- types.ts
+++ types.ts
@@ -5,3 +5,23 @@ export interface Item {
     vendorLink?: string;
 }
 
+
+export interface Beverage {
+    id: number;
+    name: string;
+    quantity: number;
+  }
+  
+  export interface Restock {
+    id: number;
+    beverageId: number;
+    dateTime: string; // Using string to represent ISO 8601 format
+    amount: number;
+  }
+  
+  export interface Count {
+    id: number;
+    beverageId: number;
+    dateTime: string; // Using string to represent ISO 8601 format
+    amount: number;
+  }
